38f1c6f9a317bd723a50e1e3e36ad88808c45a24
WtsCrackin <galomeggiolarobul@gmail.com>
2022-06-21 11:03:10 -0300

    creating base directory

---
+++ src/components/landing/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Landing = () => {
+    return (
+        <>
+            <div>
+                <h1>Galo Meggiolaro</h1>
+                <h2>Front-End Developer</h2>
+            </div>
+        </>
+    )
+}
+
+export default Landing
---
+++ src/components/landing/LandingContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Landing from './Landing'
+
+const LandingContainer = () => {
+    return (
+        <section>
+            <Landing />
+        </section>
+    )
+}
+
+export default LandingContainer
